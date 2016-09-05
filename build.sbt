073da437f9f099d09e8bacaf5991e24054549f27
muszynskim <MuszynskiM@quindell.com>
2016-09-05 23:39:12 +0100

    monoid

--- build.sbt
+++ build.sbt
@@ -2,11 +2,15 @@ name := "scala-fp"
 
 version := "1.0"
 
-scalaVersion := "2.11.4"
+scalaVersion := "2.11.8"
 
 isSnapshot := true
 
 libraryDependencies += "org.specs2" %% "specs2" % "2.3.12"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+
+libraryDependencies += "org.typelevel" %% "cats" % "0.7.2"
+
+initialCommands in console += "import cats._;import cats.instances.all._"
 
--- src/main/scala/com/sfp/MonoidDemo.scala
+++ src/main/scala/com/sfp/MonoidDemo.scala
@@ -1,5 +1,34 @@
 package com.sfp
 
-object MonoidDemo {
+import cats.implicits._
+import cats.kernel.Monoid
 
+object MonoidDemo extends App {
+
+  class MyMonoid extends Monoid[Int] {
+    override def empty: Int = 1
+    override def combine(x: Int, y: Int): Int = x * y
+  }
+
+  class MyStringMonoid extends Monoid[String] {
+    override def empty: String = ""
+    override def combine(x: String, y: String): String = x + "|" + y
+  }
+
+  def tryFoldMap: Boolean = {
+    val l = List(1, 2, 3, 4, 5)
+    val m = new MyMonoid
+    val ms = new MyStringMonoid
+    (l.foldMap(i => i.toString)(ms) == "|1|2|3|4|5") &&
+    (l.foldMap(identity)(m) == 1*2*3*4*5)
+  }
+
+  def tryCombineAll: Boolean = {
+    val l = List(1, 2, 3, 4, 5)
+    val m = new MyMonoid
+    l.combineAll(m) == 120
+  }
+
+  assert(tryFoldMap)
+  assert(tryCombineAll)
 }
